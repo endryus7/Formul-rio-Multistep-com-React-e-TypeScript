97a2e39021bc70fcaaebde623607e383e97d0049
Endryus-Montezano <endryusmontezano@live.com>
2026-03-01 15:40:51 -0300

    Estrutura do componente base

--- multistep com react e typescript/src/App.tsx
+++ multistep com react e typescript/src/App.tsx
@@ -1,4 +1,13 @@
+// Components
+import UseForm from "./components/UseForm"
+import ReviewForm from "./components/ReviewForm"
+import Thanks from "./components/Thanks"
 
+import {GrFormNext, GrFormPrevious} from "react-icons/gr"
+
+// Hooks
+
+// CSS
 import './App.css'
 
 function App() {
@@ -6,7 +15,28 @@ function App() {
 
   return (
     <div className="app">
-      <h1>React TS multistep form</h1>
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
+      </div>
+      <div className="form-container">
+        <p>passos</p>
+        <form>
+          <div className="inputs-container">
+            <UseForm />
+          </div>
+          <div className="actions">
+            <button>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
